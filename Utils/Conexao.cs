cfe3bb702cf44c075557044c392a6b2a121a4dd6
Pe-HLeite <pedrohleite28@gmail.com>
2025-07-30 16:53:01 -0300

    Conectando o banco de dados

---
+++ Utils/Conexao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuLateralHamburgueria.Utils
+{
+    public static class Conexao
+    {
+        private static string dadosConexao = @"Data Source=MAR0625654W10-1;Initial Catalog=lanchonete;
+            User ID=sa;Password=senac2025;
+            TrustServerCertificate=True;Integrated Security=false";
+
+        public static SqlConnection ObterConexao()
+        {
+            return new SqlConnection(dadosConexao);
+        }
+    }
+}
---
+++ Views/testeConexao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MenuLateralHamburgueria.Utils;
+
+namespace MenuLateralHamburgueria.Views
+{
+    public partial class testeConexao : Form
+    {
+        public testeConexao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestaConexao_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Estou aqui no Testar conexão");
+            try
+            {
+                SqlConnection conexao = Conexao.ObterConexao();
+
+                conexao.Open();
+
+                MessageBox.Show("Conexão realizada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"Erro ao tentar conectar ao banco {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
