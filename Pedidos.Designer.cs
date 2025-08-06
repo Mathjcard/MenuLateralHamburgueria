26594b3306c1bd917ace3424d93b4120e8ce53f7
Tais <tais.sferreira5@senacsp.edu.br>
2025-08-06 17:11:45 -0300

    feito a tela já com pedidos

--- Pedidos.Designer.cs
+++ Pedidos.Designer.cs
@@ -32,7 +32,36 @@
             this.panelLogo = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnEntrar = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbl12NmGarcom = new System.Windows.Forms.Label();
+            this.Lbl8HrPddo = new System.Windows.Forms.Label();
+            this.TxtBox7Bebida = new System.Windows.Forms.TextBox();
+            this.TxtBox8Lanche = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbl10Mesa02 = new System.Windows.Forms.Label();
+            this.lbl11Pdd02 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Lbl12NomeGarçom = new System.Windows.Forms.Label();
+            this.Lbl3HrPddo = new System.Windows.Forms.Label();
+            this.TxtBox3Lanche = new System.Windows.Forms.TextBox();
+            this.TxtBox4Lanche = new System.Windows.Forms.TextBox();
+            this.TxtBox5Lanche = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Lbl6Mesa05 = new System.Windows.Forms.Label();
+            this.Lbl7Pddo03 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl12NomeGarcom = new System.Windows.Forms.Label();
+            this.lbl2HrPddo = new System.Windows.Forms.Label();
+            this.tXtBox2bebida = new System.Windows.Forms.TextBox();
+            this.txt2PddCoca = new System.Windows.Forms.TextBox();
+            this.tbox1Lanche = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl03Mesa01 = new System.Windows.Forms.Label();
+            this.lbl2pedido01 = new System.Windows.Forms.Label();
             this.panelLogo.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelLogo
@@ -42,7 +71,7 @@
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(800, 67);
+            this.panelLogo.Size = new System.Drawing.Size(876, 67);
             this.panelLogo.TabIndex = 5;
             // 
             // label1
@@ -72,11 +101,284 @@
             this.btnEntrar.Text = "Novo Pedido";
             this.btnEntrar.UseVisualStyleBackColor = false;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lbl12NmGarcom);
+            this.panel3.Controls.Add(this.Lbl8HrPddo);
+            this.panel3.Controls.Add(this.TxtBox7Bebida);
+            this.panel3.Controls.Add(this.TxtBox8Lanche);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.lbl10Mesa02);
+            this.panel3.Controls.Add(this.lbl11Pdd02);
+            this.panel3.Location = new System.Drawing.Point(145, 214);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(710, 123);
+            this.panel3.TabIndex = 22;
+            // 
+            // lbl12NmGarcom
+            // 
+            this.lbl12NmGarcom.AutoSize = true;
+            this.lbl12NmGarcom.Location = new System.Drawing.Point(307, 18);
+            this.lbl12NmGarcom.Name = "lbl12NmGarcom";
+            this.lbl12NmGarcom.Size = new System.Drawing.Size(59, 13);
+            this.lbl12NmGarcom.TabIndex = 7;
+            this.lbl12NmGarcom.Text = "Garçom: Vi";
+            this.lbl12NmGarcom.Click += new System.EventHandler(this.lbl12NmGarcom_Click);
+            // 
+            // Lbl8HrPddo
+            // 
+            this.Lbl8HrPddo.AutoSize = true;
+            this.Lbl8HrPddo.Location = new System.Drawing.Point(564, 5);
+            this.Lbl8HrPddo.Name = "Lbl8HrPddo";
+            this.Lbl8HrPddo.Size = new System.Drawing.Size(123, 13);
+            this.Lbl8HrPddo.TabIndex = 6;
+            this.Lbl8HrPddo.Text = "19h37min        21/08/25";
+            this.Lbl8HrPddo.Click += new System.EventHandler(this.Lbl8HrPddo_Click);
+            // 
+            // TxtBox7Bebida
+            // 
+            this.TxtBox7Bebida.Location = new System.Drawing.Point(37, 83);
+            this.TxtBox7Bebida.Name = "TxtBox7Bebida";
+            this.TxtBox7Bebida.Size = new System.Drawing.Size(88, 20);
+            this.TxtBox7Bebida.TabIndex = 4;
+            this.TxtBox7Bebida.Text = "1x SPRITE";
+            this.TxtBox7Bebida.TextChanged += new System.EventHandler(this.TxtBox7Bebida_TextChanged);
+            // 
+            // TxtBox8Lanche
+            // 
+            this.TxtBox8Lanche.BackColor = System.Drawing.SystemColors.Control;
+            this.TxtBox8Lanche.Location = new System.Drawing.Point(37, 57);
+            this.TxtBox8Lanche.Name = "TxtBox8Lanche";
+            this.TxtBox8Lanche.Size = new System.Drawing.Size(76, 20);
+            this.TxtBox8Lanche.TabIndex = 3;
+            this.TxtBox8Lanche.Text = "1 x MILÃO";
+            this.TxtBox8Lanche.TextChanged += new System.EventHandler(this.TxtBox8Lanche_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 2;
+            // 
+            // lbl10Mesa02
+            // 
+            this.lbl10Mesa02.AutoSize = true;
+            this.lbl10Mesa02.BackColor = System.Drawing.Color.Orange;
+            this.lbl10Mesa02.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl10Mesa02.Location = new System.Drawing.Point(3, 18);
+            this.lbl10Mesa02.Name = "lbl10Mesa02";
+            this.lbl10Mesa02.Size = new System.Drawing.Size(55, 13);
+            this.lbl10Mesa02.TabIndex = 1;
+            this.lbl10Mesa02.Text = "Mesa 02";
+            // 
+            // lbl11Pdd02
+            // 
+            this.lbl11Pdd02.AutoSize = true;
+            this.lbl11Pdd02.BackColor = System.Drawing.Color.Orange;
+            this.lbl11Pdd02.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl11Pdd02.Location = new System.Drawing.Point(3, 0);
+            this.lbl11Pdd02.Name = "lbl11Pdd02";
+            this.lbl11Pdd02.Size = new System.Drawing.Size(64, 13);
+            this.lbl11Pdd02.TabIndex = 0;
+            this.lbl11Pdd02.Text = "Pedido 02";
+            this.lbl11Pdd02.Click += new System.EventHandler(this.lbl11Pdd02_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Lbl12NomeGarçom);
+            this.panel2.Controls.Add(this.Lbl3HrPddo);
+            this.panel2.Controls.Add(this.TxtBox3Lanche);
+            this.panel2.Controls.Add(this.TxtBox4Lanche);
+            this.panel2.Controls.Add(this.TxtBox5Lanche);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.Lbl6Mesa05);
+            this.panel2.Controls.Add(this.Lbl7Pddo03);
+            this.panel2.Location = new System.Drawing.Point(145, 343);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(710, 131);
+            this.panel2.TabIndex = 21;
+            // 
+            // Lbl12NomeGarçom
+            // 
+            this.Lbl12NomeGarçom.AutoSize = true;
+            this.Lbl12NomeGarçom.Location = new System.Drawing.Point(307, 19);
+            this.Lbl12NomeGarçom.Name = "Lbl12NomeGarçom";
+            this.Lbl12NomeGarçom.Size = new System.Drawing.Size(69, 13);
+            this.Lbl12NomeGarçom.TabIndex = 7;
+            this.Lbl12NomeGarçom.Text = "Garçom: Luiz";
+            // 
+            // Lbl3HrPddo
+            // 
+            this.Lbl3HrPddo.AutoSize = true;
+            this.Lbl3HrPddo.Location = new System.Drawing.Point(564, 5);
+            this.Lbl3HrPddo.Name = "Lbl3HrPddo";
+            this.Lbl3HrPddo.Size = new System.Drawing.Size(123, 13);
+            this.Lbl3HrPddo.TabIndex = 6;
+            this.Lbl3HrPddo.Text = "19h40min        21/08/25";
+            // 
+            // TxtBox3Lanche
+            // 
+            this.TxtBox3Lanche.Location = new System.Drawing.Point(36, 96);
+            this.TxtBox3Lanche.Name = "TxtBox3Lanche";
+            this.TxtBox3Lanche.Size = new System.Drawing.Size(110, 20);
+            this.TxtBox3Lanche.TabIndex = 5;
+            this.TxtBox3Lanche.Text = "2 x Suco Laranja 1L";
+            this.TxtBox3Lanche.TextChanged += new System.EventHandler(this.TxtBox3Lanche_TextChanged);
+            // 
+            // TxtBox4Lanche
+            // 
+            this.TxtBox4Lanche.Location = new System.Drawing.Point(37, 70);
+            this.TxtBox4Lanche.Name = "TxtBox4Lanche";
+            this.TxtBox4Lanche.Size = new System.Drawing.Size(88, 20);
+            this.TxtBox4Lanche.TabIndex = 4;
+            this.TxtBox4Lanche.Text = "1x MAIMI";
+            this.TxtBox4Lanche.TextChanged += new System.EventHandler(this.TxtBox4Lanche_TextChanged);
+            // 
+            // TxtBox5Lanche
+            // 
+            this.TxtBox5Lanche.BackColor = System.Drawing.SystemColors.Control;
+            this.TxtBox5Lanche.Location = new System.Drawing.Point(37, 44);
+            this.TxtBox5Lanche.Name = "TxtBox5Lanche";
+            this.TxtBox5Lanche.Size = new System.Drawing.Size(88, 20);
+            this.TxtBox5Lanche.TabIndex = 3;
+            this.TxtBox5Lanche.Text = "2x AMAZONAS";
+            this.TxtBox5Lanche.TextChanged += new System.EventHandler(this.TxtBox5Lanche_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 2;
+            // 
+            // Lbl6Mesa05
+            // 
+            this.Lbl6Mesa05.AutoSize = true;
+            this.Lbl6Mesa05.BackColor = System.Drawing.Color.Orange;
+            this.Lbl6Mesa05.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl6Mesa05.Location = new System.Drawing.Point(91, 5);
+            this.Lbl6Mesa05.Name = "Lbl6Mesa05";
+            this.Lbl6Mesa05.Size = new System.Drawing.Size(55, 13);
+            this.Lbl6Mesa05.TabIndex = 1;
+            this.Lbl6Mesa05.Text = "Mesa 05";
+            // 
+            // Lbl7Pddo03
+            // 
+            this.Lbl7Pddo03.AutoSize = true;
+            this.Lbl7Pddo03.BackColor = System.Drawing.Color.Orange;
+            this.Lbl7Pddo03.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl7Pddo03.Location = new System.Drawing.Point(3, 5);
+            this.Lbl7Pddo03.Name = "Lbl7Pddo03";
+            this.Lbl7Pddo03.Size = new System.Drawing.Size(64, 13);
+            this.Lbl7Pddo03.TabIndex = 0;
+            this.Lbl7Pddo03.Text = "Pedido 03";
+            this.Lbl7Pddo03.Click += new System.EventHandler(this.Lbl7Pddo03_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl12NomeGarcom);
+            this.panel1.Controls.Add(this.lbl2HrPddo);
+            this.panel1.Controls.Add(this.tXtBox2bebida);
+            this.panel1.Controls.Add(this.txt2PddCoca);
+            this.panel1.Controls.Add(this.tbox1Lanche);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lbl03Mesa01);
+            this.panel1.Controls.Add(this.lbl2pedido01);
+            this.panel1.Location = new System.Drawing.Point(145, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 121);
+            this.panel1.TabIndex = 20;
+            // 
+            // lbl12NomeGarcom
+            // 
+            this.lbl12NomeGarcom.AutoSize = true;
+            this.lbl12NomeGarcom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl12NomeGarcom.Location = new System.Drawing.Point(300, 10);
+            this.lbl12NomeGarcom.Name = "lbl12NomeGarcom";
+            this.lbl12NomeGarcom.Size = new System.Drawing.Size(72, 15);
+            this.lbl12NomeGarcom.TabIndex = 7;
+            this.lbl12NomeGarcom.Text = "Garçom: Tais";
+            this.lbl12NomeGarcom.Click += new System.EventHandler(this.lbl12NomeGarcom_Click);
+            // 
+            // lbl2HrPddo
+            // 
+            this.lbl2HrPddo.AutoSize = true;
+            this.lbl2HrPddo.Location = new System.Drawing.Point(592, 5);
+            this.lbl2HrPddo.Name = "lbl2HrPddo";
+            this.lbl2HrPddo.Size = new System.Drawing.Size(95, 13);
+            this.lbl2HrPddo.TabIndex = 6;
+            this.lbl2HrPddo.Text = "19h        21/08/25";
+            this.lbl2HrPddo.Click += new System.EventHandler(this.lbl2HrPddo_Click);
+            // 
+            // tXtBox2bebida
+            // 
+            this.tXtBox2bebida.Location = new System.Drawing.Point(36, 96);
+            this.tXtBox2bebida.Name = "tXtBox2bebida";
+            this.tXtBox2bebida.Size = new System.Drawing.Size(110, 20);
+            this.tXtBox2bebida.TabIndex = 5;
+            this.tXtBox2bebida.Text = "1 x Suco Laranja 1L";
+            this.tXtBox2bebida.TextChanged += new System.EventHandler(this.tXtBox2bebida_TextChanged);
+            // 
+            // txt2PddCoca
+            // 
+            this.txt2PddCoca.Location = new System.Drawing.Point(37, 70);
+            this.txt2PddCoca.Name = "txt2PddCoca";
+            this.txt2PddCoca.Size = new System.Drawing.Size(88, 20);
+            this.txt2PddCoca.TabIndex = 4;
+            this.txt2PddCoca.Text = "1x COCA-COLA 2L";
+            // 
+            // tbox1Lanche
+            // 
+            this.tbox1Lanche.BackColor = System.Drawing.SystemColors.Control;
+            this.tbox1Lanche.Location = new System.Drawing.Point(37, 44);
+            this.tbox1Lanche.Name = "tbox1Lanche";
+            this.tbox1Lanche.Size = new System.Drawing.Size(76, 20);
+            this.tbox1Lanche.TabIndex = 3;
+            this.tbox1Lanche.Text = "1 x DETROIT";
+            this.tbox1Lanche.TextChanged += new System.EventHandler(this.tbox1Lanche_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 2;
+            // 
+            // lbl03Mesa01
+            // 
+            this.lbl03Mesa01.AutoSize = true;
+            this.lbl03Mesa01.BackColor = System.Drawing.Color.Orange;
+            this.lbl03Mesa01.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl03Mesa01.Location = new System.Drawing.Point(91, 5);
+            this.lbl03Mesa01.Name = "lbl03Mesa01";
+            this.lbl03Mesa01.Size = new System.Drawing.Size(55, 13);
+            this.lbl03Mesa01.TabIndex = 1;
+            this.lbl03Mesa01.Text = "Mesa 01";
+            // 
+            // lbl2pedido01
+            // 
+            this.lbl2pedido01.AutoSize = true;
+            this.lbl2pedido01.BackColor = System.Drawing.Color.Orange;
+            this.lbl2pedido01.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2pedido01.Location = new System.Drawing.Point(3, 5);
+            this.lbl2pedido01.Name = "lbl2pedido01";
+            this.lbl2pedido01.Size = new System.Drawing.Size(64, 13);
+            this.lbl2pedido01.TabIndex = 0;
+            this.lbl2pedido01.Text = "Pedido 01";
+            this.lbl2pedido01.Click += new System.EventHandler(this.lbl2pedido01_Click);
+            // 
             // frmPedidos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(876, 671);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnEntrar);
             this.Controls.Add(this.panelLogo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -84,6 +386,12 @@
             this.Text = "PEDIDOS";
             this.panelLogo.ResumeLayout(false);
             this.panelLogo.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -93,5 +401,31 @@
         private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lbl12NmGarcom;
+        private System.Windows.Forms.Label Lbl8HrPddo;
+        private System.Windows.Forms.TextBox TxtBox7Bebida;
+        private System.Windows.Forms.TextBox TxtBox8Lanche;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbl10Mesa02;
+        private System.Windows.Forms.Label lbl11Pdd02;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label Lbl12NomeGarçom;
+        private System.Windows.Forms.Label Lbl3HrPddo;
+        private System.Windows.Forms.TextBox TxtBox3Lanche;
+        private System.Windows.Forms.TextBox TxtBox4Lanche;
+        private System.Windows.Forms.TextBox TxtBox5Lanche;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Lbl6Mesa05;
+        private System.Windows.Forms.Label Lbl7Pddo03;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl12NomeGarcom;
+        private System.Windows.Forms.Label lbl2HrPddo;
+        private System.Windows.Forms.TextBox tXtBox2bebida;
+        private System.Windows.Forms.TextBox txt2PddCoca;
+        private System.Windows.Forms.TextBox tbox1Lanche;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl03Mesa01;
+        private System.Windows.Forms.Label lbl2pedido01;
     }
 }
--- Pedidos.cs
+++ Pedidos.cs
@@ -16,5 +16,75 @@ namespace MenuLateralHamburgueria
         {
             InitializeComponent();
         }
+
+        private void lbl2pedido01_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbox1Lanche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tXtBox2bebida_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl12NomeGarcom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl2HrPddo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl11Pdd02_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBox8Lanche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBox7Bebida_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl12NmGarcom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl8HrPddo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl7Pddo03_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBox5Lanche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBox4Lanche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBox3Lanche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
